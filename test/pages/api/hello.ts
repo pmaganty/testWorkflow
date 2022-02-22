fd431f7f27edf9248cf94659cb3cd4de02083fa2
Pranitha Maganty <pmaganty@bl-pmaganty-vtrqd257pg.attlocal.net>
2022-02-22 07:18:03 -0600

    git workflow

--- test/pages/api/hello.ts
+++ test/pages/api/hello.ts
@@ -1,14 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  console.log("hello");
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'POST') {
+    try {
+      const { authorization } = req.headers;
+
+      if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
+        res.status(200).json({ success: true });
+        console.log("good");
+      } else {
+        res.status(401).json({ success: false });
+        console.log("bad");
+      }
+    } catch (err) {
+      res.status(500).json({ statusCode: 500 });
+      console.log("server err");
+    }
+  } else {
+    //res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+    console.log("not allowed");
+  }
 }
